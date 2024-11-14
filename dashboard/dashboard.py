b7de9c006b6c4a9fd7a51675aa41a9654db60638
dyahtri <dyahttari@gmail.com>
2024-11-15 00:24:12 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -68,6 +68,7 @@ elif menu == "Visualisasi":
     plt.plot(daily_counts.index, daily_counts['cnt'], color='green', linewidth=2)
     plt.xlabel("Tanggal")
     plt.ylabel("Jumlah Penyewaan Sepeda (cnt)")
+    plt.xticks([0, 1, 2, 3], ['Clear/Few clouds', 'Mist/Cloudy', 'Light Snow/Light Rain', 'Heavy Rain/Ice'])
     plt.title("Tren Penyewaan Sepeda Harian")
     st.pyplot(plt)
 
